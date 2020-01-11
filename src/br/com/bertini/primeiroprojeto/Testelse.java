92fb02213653f5d62c3b49ac113c160096caacf7
GabrielGF4 <gabriel_antonio_gfa@hotmail.com>
2020-01-11 16:16:53 -0300

    teste

--- src/br/com/bertini/primeiroprojeto/Testelse.java
+++ src/br/com/bertini/primeiroprojeto/Testelse.java
@@ -4,14 +4,15 @@ public class Testelse {
 
 	public static void main(String[] args) {
 		
-		int idade = 15;
-		boolean amigoDoDono = true;
+		int idade = 19;
+		boolean amigoDoDono = false;
 		
-		if (idade > 18) {
+		if (idade > 18 && amigoDoDono) {
 			System.out.println("entra");
 		} else if(idade == 15){
 			System.out.println("Não pode entrar");
 		} else {
+			System.out.println("não entra");
 			System.out.println("não entra");
 		}
 	
