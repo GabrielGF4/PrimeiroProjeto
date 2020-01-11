b80371c3f8050d7159a07ca680ab98d998a54ec4
GabrielGF4 <gabriel_antonio_gfa@hotmail.com>
2020-01-11 16:11:32 -0300

    commit inicial do projeto

---
+++ src/br/com/bertini/primeiroprojeto/TesteComparacaoTiposVariaveis.java
@@ -0,0 +1,22 @@
+package br.com.bertini.primeiroprojeto;
+
+public class TesteComparacaoTiposVariaveis {
+
+	public static void main(String[] args) {
+		
+		Cliente cliente = new Cliente();
+		
+		System.out.println("Primitivos");
+		System.out.println(cliente.getIdadePrimitivo());
+		System.out.println(cliente.isClienteAtivoPrimitivo());
+		System.out.println(cliente.getValorPrimitivo());
+		
+		System.out.println("..............");
+		
+		System.out.println("WRAPPERS");
+		System.out.println(cliente.getIdadeWrapper());
+		System.out.println(cliente.getClienteAtivoWrapper());
+		System.out.println(cliente.getValorWrapper());
+	}
+
+}
---
+++ src/br/com/bertini/primeiroprojeto/Testelse.java
@@ -0,0 +1,19 @@
+package br.com.bertini.primeiroprojeto;
+
+public class Testelse {
+
+	public static void main(String[] args) {
+		
+		int idade = 15;
+		boolean amigoDoDono = true;
+		
+		if (idade > 18) {
+			System.out.println("entra");
+		} else if(idade == 15){
+			System.out.println("Não pode entrar");
+		} else {
+			System.out.println("não entra");
+		}
+	
+	}
+}
